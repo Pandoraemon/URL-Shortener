d14aed18ca8e2aa4f566d14eb65da280d90f9702
Pandoraemon <pz880304@163.com>
2016-08-01 22:45:31 +0800

    add reg of url validator

--- index.py
+++ index.py
@@ -1,10 +1,27 @@
 from flask import Flask
+import re
 
 app = Flask(__name__)
+regex = re.compile(
+        r'^((http|https|)?://)?'
+        r'(([0-9]{1,3}\.){3}[0-9]{1-3})|'
+        r'(((www\.)?[0-9a-z][0-9a-z-]{0,61})?[0-9a-z]\.'
+        r'[a-z]{2,6})(:[0-9]{1,4})?'
+        r'((/?)|(/[0-9a-z_!~*\'().:?;@&=+$,%#-]+)+/?)$',
+        re.IGNORECASE)
+
+
 
 @app.route('/')
 def index():
     return '<h1>Hello World!</h1>'
+
+@app.route('/new/<path:url>')
+def validate(url):
+    if re.search(regex, url) is not None:
+        return url
+    else:
+        return "Invalid URL"
 
 if __name__ == '__main__':
     app.run(debug=True)
